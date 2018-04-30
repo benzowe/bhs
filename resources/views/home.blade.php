82dcc595b7a6ceedc0cb34b9e9b8d23b481e8ce8
benzowe <muhammeds90@yahoo.com>
2018-05-01 00:09:08 +0100

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        bhs
+        <img src="http://res.cloudinary.com/benzowe/image/upload/v1525121746/bhslogo.jpg" alt="#">
+    </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+<style>
+body {
+    margin:0;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: beige;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.navbar a {
+  float: left;
+  display: block;
+  color: black;
+  font-family:monospace;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.navbar a:hover {
+  background: #ddd;
+  color: black;
+}
+
+.main {
+    
+  padding: px;
+  margin-top: 200px;
+  height: 1500px; /* Used in this example to enable scrolling */
+}
+.one{
+    background-image: url(http://res.cloudinary.com/benzowe/image/upload/v1525125672/foggy.png);
+}
+.search{
+    padding: 16px;
+    margin-top: 20px;
+}
+input[type=text]{
+    width: 100%;
+    padding:0;
+    height: 40px;
+    border-radius: 3px;
+    background-color: beige;
+    border-color: black;
+    color: black;
+    float: left;
+    overflow: hidden;
+    font-family: cursive;
+    font-size: 15px;
+    margin: 0;
+}
+
+.search { 
+    position: relative; }
+.search input { 
+    text-indent: 30px;}
+.search .fa-search { 
+  position: absolute;
+  top: 35px;
+  left: 30px;
+  font-size: 15px;}
+</style>
+</head>
+<body>
+
+<div class="navbar">
+    <h1>B. H. S Signatures</h1>
+  <a href="#home">HOME.</a>
+  <a href="#news">NEWS.</a>
+  <a href="#contact">CONTACT.</a>
+  <a href=""><i class="fas fa-shopping-cart"></i></a>
+  <br>
+        <div class="search">
+        <table border="0" width="100%">
+         <tr>
+            <td>
+                    
+            <input type="text" placeholder="S E A R C H">  
+            <span>
+            <i class="fas fa-search"></i>    
+            </span>     
+            </td>     
+        </tr>   
+        </table>
+        </div>
+</div>
+
+<div class="main">
+   
+      <table border="0" width="100%">
+        <tr class="one">
+            <td>
+
+               <center> <h1 style="font-family:Verdana, Geneva, Tahoma, sans-serif;color:#1a1a00;padding:30px">DISCOVER</h1><H2 style="font-family:cursive;color:#1a1a00">Pure African Clothing</H2><a style="text-decoration:none" href=""><h3 style="font-family:Georgia, 'Times New Roman', Times, serif;color:#1a1a00">SHOP NOW</h3></a></center>
+            </td>
+        </tr>
+        <tr>
+            <td><br>
+                <table border="1" width="100%"align="center">
+                    <tr>
+                        <td align="center">
+                            <img src="http://res.cloudinary.com/benzowe/image/upload/v1525121746/bhslogo.jpg" alt="buuh" width="300" height="300"><br>
+                            <h1>Kente</h1>
+                            <h6>SHOP NOW</h6>
+                        </td>
+                        <td align="center">
+                                <img src="http://res.cloudinary.com/benzowe/image/upload/v1525121746/bhslogo.jpg" alt="buuuh" width="300" height="300"><br>
+                                <h1>PEPLON</h1>
+                                <h6>SHOP NOW</h6>
+                        </td>
+                        <td align="center">
+                                <img src="http://res.cloudinary.com/benzowe/image/upload/v1525121746/bhslogo.jpg" alt="buuuh" width="300" height="300"><br>
+                                <h1>PEPLON</h1>
+                                <h6>SHOP NOW</h6>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            <img src="http://res.cloudinary.com/benzowe/image/upload/a_hflip/v1525127827/IMG_0626.jpg" alt="Buuh" width="100" height="200">
+                            <h3 style="font-family:American Typrewriter">Shop For Women</h3>
+                        </td>
+                        <td align="center">
+                                <img src="http://res.cloudinary.com/benzowe/image/upload/v1523637805/IMG_0343.jpg" alt="Buuh" width="100" height="200">
+                                <h3 style="font-family:American Typrewriter">Shop For Men</h3>
+                            </td>
+                            <td align="center">
+                                    <img src="http://res.cloudinary.com/benzowe/image/upload/a_hflip/v1525127786/IMG_0631.jpg" alt="Buuh" width="100" height="200">
+                                    <h3 style="font-family:American Typrewriter">Shop For Kids</h3>
+                                </td>
+                                
+                    </tr>
+                </table>
+                <hr>
+            </td>
+        </tr>
+      </table>
+
+        <table border="1" width="100%">
+            <tr>
+                <td>
+                    <button>
+                        
+                    </button>
+                </td>
+            </tr>
+        </table>
+
+</div>
+
+</body>
+</html>
